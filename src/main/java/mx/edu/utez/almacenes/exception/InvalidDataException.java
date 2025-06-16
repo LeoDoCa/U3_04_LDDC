ce962e6b9605deca871e25e68a36ffdaa080693f
LeoDoCa <20223tn049@utez.edu.mx>
2025-06-16 16:47:53 -0600

    [UPDATE] Eliminación de todo

--- src/main/java/mx/edu/utez/almacenes/exception/InvalidDataException.java
+++
@@ -1,8 +0,0 @@
-package mx.edu.utez.almacenes.exception;
-
-public class InvalidDataException extends RuntimeException {
-
-    public InvalidDataException(String message) {
-        super(message);
-    }
-}
--- src/main/java/mx/edu/utez/almacenes/models/AlmacenSize.java
+++
@@ -1,7 +0,0 @@
-package mx.edu.utez.almacenes.models;
-
-public enum AlmacenSize {
-    SMALL,
-    MEDIUM,
-    LARGE
-}
